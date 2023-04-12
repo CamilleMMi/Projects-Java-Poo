56b0d7863a2711964734636c2792ec9184fd34fc
Camille <111974733+CamilleMMi@users.noreply.github.com>
2023-04-12 10:57:01 +0200

    Exercice 1 -> Reversed String

---
+++ exercice1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class exercice1 {
+    public static void main(String[] args) {
+        System.out.print("Saisissez une phrase : ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String reversedString = reverseString(input);
+        System.out.println(reversedString);
+    }
+
+    public static String reverseString(String string) {
+        String reversedString = "";
+        for (int x = string.length() - 1; x >= 0; x = x - 1) {
+            reversedString += string.charAt(x);
+        }
+        return reversedString;
+    }
+}
