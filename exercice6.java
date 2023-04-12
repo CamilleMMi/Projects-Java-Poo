42875eedff846225f9a951eddcf301d1071f9d30
Camille <111974733+CamilleMMi@users.noreply.github.com>
2023-04-12 14:37:02 +0200

    Exercice 6 -> Count Words

---
+++ exercice6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class exercice6 {
+    public static void main(String[] args) {
+        String stringInput;
+        int numberWords;
+
+        System.out.println("Choisissez une string de mots : ");
+        Scanner scanner = new Scanner(System.in);
+        stringInput = scanner.nextLine();
+
+        numberWords = countWords(stringInput);
+        System.out.println(numberWords + " Mots");
+    }
+
+    public static int countWords(String stringInput) {
+        if (stringInput.isEmpty())
+            return 0;
+        String[] splitWords = stringInput.split("\\s+");
+        return splitWords.length;
+    }
+}
