7dc87208d9e46c4dcbc4847673f69863902fc6a2
Camille <111974733+CamilleMMi@users.noreply.github.com>
2023-04-12 10:57:30 +0200

    Exercice 2 -> Print Pyramid

---
+++ exercice2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class exercice2 {
+    public static void main(String[] args) {
+        int size;
+
+        System.out.print("Choisissez une taille : ");
+        Scanner scanner = new Scanner(System.in);
+        size = scanner.nextInt();
+        printPyramid(size);
+    }
+    public static void printPyramid(int size) {
+        for (int x = 1; x <= size; x += 1) {
+            for (int y = 1; y <= size - x; y += 1) {
+                System.out.print(" ");
+            }
+            for (int y = 1; y <= 2 * x - 1; y += 1) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
