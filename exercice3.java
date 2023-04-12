87e584fcf6c9884231e7fe0e9a0626423b930569
Camille <111974733+CamilleMMi@users.noreply.github.com>
2023-04-12 11:31:14 +0200

    Exercice 3 -> Sum to 10 000

--- exercice3.java
+++ exercice3.java
@@ -1,7 +1,7 @@
 public class exercice3 {
     public static void main(String[] args) {
         int total = 1;
-        for (int x = 1; total < 10000; x += 1) {
+        for (int x = 1; x < 10000; x += 1) {
             total += x;
         }
         System.out.println(total);
