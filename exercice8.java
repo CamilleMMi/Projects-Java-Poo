bead4efdf362572a384d8e1b16e24e247cc8b745
Camille <111974733+CamilleMMi@users.noreply.github.com>
2023-04-12 15:04:46 +0200

    Exercice 8 -> Count Words in File

---
+++ exercice8.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class exercice8 {
+    public static void main(String[] args) {
+        String path;
+        int count;
+
+        System.out.println("Entrez le chemin du fichier : ");
+        Scanner scanner = new Scanner(System.in);
+
+        path = scanner.nextLine();
+        count = countWordsInFile(path);
+
+        if (count != -1) {
+            System.out.println(count + " Mots dans le fichier");
+        }
+    }
+
+    public static int countWordsInFile(String path) {
+        try {
+            File file;
+            int count = 0;
+
+            file = new File(path);
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNext()) {
+                String ligne = scanner.nextLine();
+                count += ligne.split("\\s+").length;
+            }
+            scanner.close();
+            return count;
+        } catch (FileNotFoundException error) {
+            System.out.println("Ahah bien essayé je le trouve pas ton fichier texte là");
+            return -1;
+        }
+    }
+}
