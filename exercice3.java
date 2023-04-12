ae2651964e920a13ff2a317fbc7832762b9d659d
Camille <111974733+CamilleMMi@users.noreply.github.com>
2023-04-12 11:26:46 +0200

    Exercice 3 -> Somme jusqu'à 10 000 ?

---
+++ exercice3.java
@@ -0,0 +1,9 @@
+public class exercice3 {
+    public static void main(String[] args) {
+        int total = 1;
+        for (int x = 1; total < 10000; x += 1) {
+            total += x;
+        }
+        System.out.println(total);
+    }
+}
