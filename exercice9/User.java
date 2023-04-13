7906986224d7559aa0570e858a5861d56ba3a202
CamilleMMi <camillemichaudmeli@gmail.com>
2023-04-13 12:01:32 +0200

    Modifications on User and Voiture entities

--- exercice9/User.java
+++ exercice9/User.java
@@ -1,27 +1,65 @@
 package projectjavajpoo.exercice9;
 
+import java.util.Date;
+
 public class User {
-    private String firstName;
-    private String lastName;
+    private int id;
 
-    public User(String lastName, String firstName) {
-        this.lastName = lastName;
-        this.firstName = firstName;
+    private String firstname;
+
+    private String lastname;
+
+    private double size;
+
+    private Date birthdate;
+
+    public User(int id, String firstname, String lastname, double size, Date birthdate) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.size = size;
+        this.birthdate = birthdate;
     }
 
-    public String getFirstName() {
-        return firstName;
+    // Getters and setters
+
+    public int getId() {
+        return id;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getFirstname() {
+        return firstname;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
     }
 }
--- exercice9/Voiture.java
+++ exercice9/Voiture.java
@@ -1,12 +1,48 @@
 package projectjavajpoo.exercice9;
 
 public class Voiture {
-    private String marque;
-    private String modele;
+    private int id;
 
-    public Voiture(String marque, String modele) {
+    private String nom;
+
+    private String model;
+
+    private String marque;
+
+    private int maxSpeed;
+
+    public Voiture(int id, String nom, String model, String marque, int maxSpeed) {
+        this.id = id;
+        this.nom = nom;
+        this.model = model;
         this.marque = marque;
-        this.modele = modele;
+        this.maxSpeed = maxSpeed;
+    }
+
+    // Getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getModele() {
+        return model;
+    }
+
+    public void setModele(String modele) {
+        this.model = modele;
     }
 
     public String getMarque() {
@@ -17,11 +53,11 @@ public class Voiture {
         this.marque = marque;
     }
 
-    public String getModele() {
-        return modele;
+    public int getVitesseMaximale() {
+        return maxSpeed;
     }
 
-    public void setModele(String modele) {
-        this.modele = modele;
+    public void setVitesseMaximale(int vitesseMaximale) {
+        this.maxSpeed = vitesseMaximale;
     }
 }
